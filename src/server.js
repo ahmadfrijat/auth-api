6a8a815920c5eefd62ab9dd564480f7108386a0c
ahmadfrijat <ahmadalfrijat7@gmail.com>
2021-04-06 19:34:55 +0300

    done

---
+++ src/server.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const notFoundHandler = require('./error-handlers/404.js');
+const errorHandler = require('./error-handlers/500.js');
+const logger = require('./middleware/logger.js');
+
+const v2Routes = require('./routes/v2.js');
+const v1Routes = require('./routes/v1.js');
+const authRoutes = require('./auth/routes.js');
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(logger);
+
+app.use('/api/v2', v2Routes);
+app.use('/api/v1', v1Routes);
+app.use(authRoutes);
+
+app.use('*', notFoundHandler);
+app.use(errorHandler);
+
+module.exports = {
+  server: app,
+  start: (port) => {
+    const PORT = port || 8080;
+    app.listen(PORT, () => console.log(`Server Up on ${PORT}`));
+  },
+};
